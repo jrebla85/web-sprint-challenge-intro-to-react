bc64480295bcddafdc1f1a13b7b12ffae562dd4a
jeremy black <jrebla85@yahoo.com>
2021-03-07 23:53:50 -0600

    Refactored code into components

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,19 +3,23 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function Character(props) {
-    const { charId, close } = props
-    const [details, setDetails] = useState(null)
+  const [character, setCharacter] = useState([]);
 
-    useEffect (() => {
-        axios.get(`https://swapi.dev/api/people/`)
-        .then(result => setDetails(result.data))
-      }, [charId])
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(result => setCharacter(result.data))
+    .catch(err => console.log(err))
+  }, [])
 
-      return (
-        <div className='container'>
-          <h2>Details of {details.name}):</h2>
-          <button onClick={close}>Close</button>
-        </div>
-      )
-
+return (
+  <ul>
+  {character.map((ch, index) => {
+    return (
+      <div key={index}>
+        {ch.name}
+      </div>
+    )
+  })}
+  </ul>
+)
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function Header() {
+
+
+    return (
+        <h1 className="Header"> Noteable Star Wars Characters</h1>
+    )
+}
