932e0f5fe0d4552e4df882f81b9f5c0efad95b5a
jeremy black <jrebla85@yahoo.com>
2021-03-08 01:04:59 -0600

    Basic Styling applied

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 export default function Character() {
 
   const [character, setCharacter] = useState([])
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const StyledHeader = styled.h1`
+    color: red;
+    font-weight: bold;
+
+    &:hover {
+        color: purple;
+        background; white;
+    }
+`
 
 export default function Header() {
 
 
     return (
-        <h1 className="Header"> Noteable Star Wars Characters</h1>
+        <StyledHeader className="Header"> Noteable Star Wars Characters</StyledHeader>
     )
 }
