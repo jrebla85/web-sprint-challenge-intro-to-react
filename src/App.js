39c1c0d956cf9feef6d25dded7a3dfdf165977d4
jeremy black <jrebla85@yahoo.com>
2021-03-07 22:40:43 -0600

    Getting Character info to display from axios call, cleanup needed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [character, setCharacter] = useState([]);
+  const [currentCharId, setCurrentCharId] = useState(null)
+
+  const openDetails = id => {
+    setCurrentCharId(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharId(null)
+  }
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  //*setup codegrade
-  //
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(result => setCharacter(result.data))
+    .catch(err => console.log(err))
+  }, [])
+
+  const Person = props => (
+    <div className="character">
+      {props.info.name}
+      <button onClick={() => openDetails(props.info.id)}>
+        Character Info
+      </button>
+    </div>
+  )
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header"> Noteable Star Wars Characters</h1>
+      {character.map((ch, index) => {
+        return (
+          <div key={index}>
+            {ch.name}
+          </div>
+        )
+      })}
+      {
+        currentCharId && <Character charId={currentCharId} close={closeDetails} />
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Character = (props) => {
+    const { charId, close } = props
+    const [details, setDetails] = useState(null)
+
+    useEffect (() => {
+        axios.get(`https://swapi.dev/api/people/`)
+        .then(result => setDetails(result.data))
+      }, [charId])
+
+      return (
+        <div className='container'>
+          <h2>Details of {charId}):</h2>
+          {
+            details &&
+            <>
+              <p>{details.name} is {details.age}</p>
+              <p>email is {details.email}</p>
+              {details.name} likes:
+              <ul>
+                {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
+              </ul>
+            </>
+          }
+          <button onClick={close}>Close</button>
+        </div>
+      )
+
+}
+
+export default Character
